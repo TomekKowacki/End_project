3355762ac9a677ebf44df1409830f005c8ec1795
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-03-23 17:48:17 +0100

    First configuration

---
+++ src/test/java/com/end_project/EndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.end_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
